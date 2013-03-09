baba68ba8171386d22c30e23fe3903520a2e702c
JacobS <jstein3@macalester.edu>
2013-03-09 13:04:49 -0800

    blah

--- grails-app/domain/User.groovy
+++ grails-app/domain/User.groovy
@@ -1,9 +1,9 @@
-/**
- * Created with IntelliJ IDEA.
- * User: JacobS
- * Date: 3/9/13
- * Time: 1:01 PM
- * To change this template use File | Settings | File Templates.
- */
 class User {
+    String userid
+    byte[] avatar
+    String avatarType
+    static constraints = {
+        avatar(nullable:true, maxSize: 16384 /* 16K */)
+        avatarType(nullable:true)
+    }
 }
